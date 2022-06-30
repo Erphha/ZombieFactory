7821d02de03c57b679620772cc889f08375fa04d
Erfan Mougahi <Erphhan@gmail.com>
2022-06-30 11:55:17 +0430

    Modifying onlyOwner

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,12 +2,22 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ZombieFactory", function () {
-  it("Should work properly!", async function () {
+  it("Factory test!", async function () {
     const ZombieFactory = await ethers.getContractFactory("ZombieFactory");
     const zombiefactory = await ZombieFactory.deploy();
     await zombiefactory.deployed();
 
     await zombiefactory.createRandomZombie("Erphha");
-    console.log('Zombie created successfully');
+    console.log("Zombie created successfully");
+  });
+
+  it("Feeding test!", async function () {
+    const ZombieFeeding = await ethers.getContractFactory("ZombieFeeding");
+    const zombiefeeding = await ZombieFeeding.deploy();
+    await zombiefeeding.deployed();
+    [owner, address1, address2] = await ethers.getSigners();
+    const owneraddress = address1.address;
+
+    await zombiefeeding.setKittyContractAddress(owneraddress);
   });
 });
